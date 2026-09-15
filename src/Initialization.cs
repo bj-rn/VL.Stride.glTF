3dfda0c28f79043dd9b6d77cc59d170cc7c2309a
bj-rn <schwarzer@posteo.net>
2026-09-15 14:50:17 +0200

    Adds VL.Stride.glTF project

---
+++ src/Initialization.cs
@@ -0,0 +1,4 @@
+// Only the Nodes namespace reaches the node browser. Core stays public for the tests.
+using VL.Core.Import;
+
+[assembly: ImportNamespace("VL.Stride.glTF.Nodes", Category = "Stride.Models")]
